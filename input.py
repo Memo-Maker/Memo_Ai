077d251ed39f1ed2bc75f74865ed44b7bac35614
Taeksin Kim <yoyo2521@naver.com>
2024-03-21 02:15:37 +0900

    File Organization

--- input.py
+++
@@ -1,6 +0,0 @@
-from pytube import YouTube
-
-yt = YouTube('https://www.youtube.com/watch?v=rd0I-sFlclw')
-
-yt.streams.filter(only_audio=True).first().download(
-    output_path='.', filename='input.mp3')
