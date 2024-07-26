1522ba9aee268e94f15adf9f4a52d0400f0cd0f8
Taeksin Kim <yoyo2521@naver.com>
2024-07-27 02:44:50 +0900

    HTTP Error 400: Bad Request 오류해결 24.07.27

--- python/Audio_STT_Summary_ras.py
+++ python/Audio_STT_Summary_ras.py
@@ -18,6 +18,11 @@ from langchain.prompts import PromptTemplate
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from dotenv import load_dotenv
 from pytube.innertube import _default_clients
+_default_clients[ "ANDROID"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients[ "ANDROID_EMBED"][ "context"][ "client"]["clientVersion"] = "19.08.35"
+_default_clients[ "IOS_EMBED"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"][ "context"]["client"]["clientVersion"] = "6.41"
 _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 # .env 파일에서 환경 변수 로드
